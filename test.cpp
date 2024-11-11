318226eb69776159f7f62e7ec6fc5d653e314b83
yhQin-gd <1701339239@qq.com>
2024-11-11 10:54:38 +0800

    来自master

---
+++ test.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "from computer"<<endl;
+   int a=1,b=2,c;
+   c = a+b;
+   cout << "c="<<c<<endl;
+   cout << "from branch test"<<endl;
+   return 0;
+}
+
